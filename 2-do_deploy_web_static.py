25e684f3883a9758df2cf407f43b6d6ef016bd4e
Innocentefea <innocentefeakpoyibo@gmail.com>
2023-10-04 14:03:03 -0700

    AirBnB_V2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from fabric.api import *
 import os
 
-env.hosts = ["52.91.121.146", "3.85.136.181"]
+env.hosts = ["54.197.45.60", "18.207.235.240"]
 env.user = "ubuntu"
 
 
